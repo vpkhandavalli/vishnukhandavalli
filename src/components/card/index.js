0891282b85ab885c76b6cd3a57eb9a07a532b4c5
Aaron Lote <5314860+azz0r@users.noreply.github.com>
2019-03-28 23:38:39 +0000

    Move styling to the right place

--- src/components/card/index.js
+++ src/components/card/index.js
@@ -2,9 +2,6 @@ import React from 'react';
 
 const defaultStyles = {
     Card: {
-        marginBottom: '1.5rem',
-        fontSize: 'inherit',
-        boxShadow: '3px -1px 6px 3px rgba(0, 0, 0, 0.1), 0 0 40px rgba(0, 0, 0, 0.1) inset',
         display: 'flex',
         overflow: 'hidden',
         justifyContent: 'center',
@@ -17,8 +14,8 @@ const defaultStyles = {
         padding: '.2rem 0.5rem',
     }
 }
-const Card = ({ children, style }) => <div style={Object.assign({}, defaultStyles.Card, style)}>{children}</div>;
+const Card = ({ children, style }) => <div tabIndex={0} style={Object.assign({}, defaultStyles.Card, style)}>{children}</div>;
 const CardActionArea = ({ children, style }) => <ul style={Object.assign({}, defaultStyles.CardActionArea, style)}>{children}</ul>;
-const CardContent = ({ children, style }) => <li style={Object.assign({}, defaultStyles.CardContent, style)}>{children}</li>;
+const CardContent = ({ children, style }) => <li tabIndex={0} style={Object.assign({}, defaultStyles.CardContent, style)}>{children}</li>;
 
 export { Card, CardActionArea, CardContent };
--- src/components/jobs/index.js
+++ src/components/jobs/index.js
@@ -3,27 +3,36 @@ import ReactMarkdown from 'react-markdown';
 
 import { Card, CardActionArea, CardContent } from '../card';
 
-const overrideStyle = [
-	{
-		CardActionArea: {
-			backgroundColor: '#503e73',
-			color: 'white'
+const overrideStyle = {
+	Card: {
+		fontSize: 'inherit',
+		marginBottom: '1.5rem',
+		boxShadow: '3px -1px 6px 3px rgba(0, 0, 0, 0.1), 0 0 40px rgba(0, 0, 0, 0.1) inset',
+		justifyContent: 'center',
+		flexDirection: 'column'
+	},
+	JobRows: [
+		{
+			CardActionArea: {
+				backgroundColor: '#503e73',
+				color: 'white'
+			},
+			CardContent: {}
 		},
-		CardContent: {}
-	},
-	{
-		CardActionArea: {
-			backgroundColor: '#a8a6a6',
-			color: 'white'
+		{
+			CardActionArea: {
+				backgroundColor: '#a8a6a6',
+				color: 'white'
+			}
+		},
+		{
+			CardActionArea: {
+				backgroundColor: 'transparent',
+				color: 'black'
+			}
 		}
-	},
-	{
-		CardActionArea: {
-			backgroundColor: 'transparent',
-			color: 'black'
-		}
-	}
-];
+	]
+};
 
 export default function Jobs({ jobs }) {
 	return (
@@ -40,8 +49,8 @@ export default function Jobs({ jobs }) {
 					to = ''
 				}) => {
 					return (
-						<Card>
-							<CardActionArea style={overrideStyle[0].CardActionArea}>
+						<Card style={overrideStyle.Card}>
+							<CardActionArea style={overrideStyle.JobRows[0].CardActionArea}>
 								<CardContent>
 									<a href={url} target="_blank" rel="noopener noreferrer">
 										{alias}
@@ -49,18 +58,18 @@ export default function Jobs({ jobs }) {
 									· {role}
 								</CardContent>
 							</CardActionArea>
-							<CardActionArea style={overrideStyle[1].CardActionArea}>
+							<CardActionArea style={overrideStyle.JobRows[1].CardActionArea}>
 								<CardContent>
 									{location} · {from} to {to}
 								</CardContent>
 							</CardActionArea>
-							<CardActionArea style={overrideStyle[2].CardActionArea}>
+							<CardActionArea style={overrideStyle.JobRows[2].CardActionArea}>
 								<CardContent>
 									{responsibilities}
 									<ul>
 										{description.map((input, index) => (
-											<li key={index}>
-												<ReactMarkdown linkTarget='_blank' source={input} />
+											<li key={index} tabIndex={0}>
+												<ReactMarkdown linkTarget="_blank" source={input} />
 											</li>
 										))}
 									</ul>
