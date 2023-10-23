6e542fe53c6a0109afc418ddd3b49d6bf04fbef7
Aaron Wuggawoo <5314860+azz0r@users.noreply.github.com>
2023-10-23 18:06:34 +0100

    chore(master): improve person projects design

--- src/components/personal-projects/index.js
+++ src/components/personal-projects/index.js
@@ -9,23 +9,25 @@ export function PersonalProjects() {
             <h2 tabIndex={0}>
                 Personal Projects
             </h2>
-            <ul>
-                <li>
+            <div>
+                <div>
                     <a href="https://fedsimulator.com" target="_blank" rel="noopener noreferrer" tabIndex={0}>
                         Fedsimulator
                     </a>{' '}
                     · A web-based platform that simulates a wrestling federation, allowing users to create shows, pay-per-view events, and television shows, as well as utilizing a draft system and other tools for free.
+                    5,000+ active monthly users.
                     <br />
                     <img src={FedSimutator} alt="Fedsimutator" style={{ maxWidth: 300 }} />
-                </li>
-                <li>
+                </div>
+                <br />
+                <div>
                     <a href="https://fedsimx.com" target="_blank" rel="noopener noreferrer" tabIndex={0}>
                         FedsimX
-                    </a>
+                    </a> - Tenth iteration
                     <br />
                     <img src={FedSimX} alt="FedsimX" style={{ maxWidth: 300 }} />
-                </li>
-            </ul>
+                </div>
+            </div>
         </>
     )
 }
